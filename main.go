d4ce16f5f54114f0374e6511a2d17f1ad827878a
yoursevg <yoursevg@gmail.com>
2024-10-27 18:15:11 +0300

    changes

--- main.go
+++ main.go
@@ -13,29 +13,29 @@ type MessageStruct struct {
 
 var message string
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		if message == "" {
-			fmt.Fprintln(w, "Hello, world!")
-		} else {
-			fmt.Fprintf(w, "Hello, %s!\n", message)
-		}
-	case "POST":
-		decoder := json.NewDecoder(r.Body)
-		var messageJSON MessageStruct
-		err := decoder.Decode(&messageJSON)
-		if err != nil {
-			panic(err)
-		}
-		message = messageJSON.Message
-		fmt.Println(message)
+func GetHelloHandler(w http.ResponseWriter, r *http.Request) {
+	if message == "" {
+		fmt.Fprintln(w, "Hello, world!")
+	} else {
+		fmt.Fprintf(w, "Hello, %s!\n", message)
 	}
+}
+
+func PostHelloHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var messageJSON MessageStruct
+	err := decoder.Decode(&messageJSON)
+	if err != nil {
+		panic(err)
+	}
+	message = messageJSON.Message
+	fmt.Fprintln(w, "New message: ", message)
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET", "POST")
+	router.HandleFunc("/api/hello", GetHelloHandler).Methods("GET")
+	router.HandleFunc("/api/hello", PostHelloHandler).Methods("POST")
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		return
